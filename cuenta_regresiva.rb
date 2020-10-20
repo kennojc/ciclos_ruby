145d92fcad60d50f5e6caea4ec8fd3701c533977
kennojc <kennojc@gmail.com>
2020-10-20 20:59:27 -0300

    Add desafío 2

---
+++ cuenta_regresiva.rb
@@ -0,0 +1,15 @@
+#Desafío reemplazar until por while
+#puts 'Ingrese un número para comenzar la cuenta: '
+#cuenta_regresiva = ARGV[0].to_i
+#puts "Contando desde #{cuenta_regresiva}..."
+#until cuenta_regresiva < 0
+#puts cuenta_regresiva
+#cuenta_regresiva -= 1
+#end
+
+cuenta_regresiva = ARGV[0].to_i
+  puts 'Ingrese un número para comenzar la cuenta: '
+  puts "Contando desde #{cuenta_regresiva}..."
+while cuenta_regresiva != 0
+  puts cuenta_regresiva-=1
+end
